27d3ae20cb8747deef2ab079decc7277c86bc49f
Pawel Juszczak <paweljuszczak@gmail.com>
2020-01-18 13:33:51 +0100

    zadanie 23

--- src/main/java/com/sda/servlet/HelloWorldServlet.java
+++ src/main/java/com/sda/servlet/HelloWorldServlet.java
@@ -1,18 +1,27 @@
 package com.sda.servlet;
 
 import javax.servlet.ServletConfig;
+import javax.servlet.annotation.WebInitParam;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-//@WebServlet(name = "HelloWorldServlet", urlPatterns = {"/"})
+@WebServlet(
+        name = "HelloWorldServlet"
+        , urlPatterns = {"/"}
+        , initParams = {
+        @WebInitParam(name = "userId", value = "123456")
+        , @WebInitParam(name = "localization", value = "Poland")
+}
+)
 public class HelloWorldServlet extends HttpServlet {
     private String varUserId, varLocalization;
 
     @Override
-    public void init(ServletConfig config){
+    public void init(ServletConfig config) {
         varUserId = config.getInitParameter("userId");
         varLocalization = config.getInitParameter("localization");
     }
