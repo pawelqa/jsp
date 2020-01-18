a9e6539536b7a8d60cef2ec4339422f74f97c943
Pawel Juszczak <paweljuszczak@gmail.com>
2020-01-18 14:26:48 +0100

    zadanie 25

--- src/main/java/com/sda/filter/MyFilter.java
+++ src/main/java/com/sda/filter/MyFilter.java
@@ -1,9 +1,12 @@
 package com.sda.filter;
 
 import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+@WebFilter(filterName = "MyFilter", urlPatterns = {"/hello"})
+//@WebFilter(filterName = "MyFilter", servletNames = {"HelloWorldServlet"})
 public class MyFilter implements Filter {
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
